c86314a60b6d6fe87a39d0e51b02b77d63962103
Devansh Behl <heydevanshbehl@gmail.com>
2023-11-16 01:36:58 +0530

    initial nextjs setup

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Navbar from '@/src/components/Navbar/Navbar';
+import './page.css'
+
+
+export default function Home() {
+
+  return (
+    <div className="app">
+    <Navbar/>
+    </div>
+  );
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="n-wrapper">
+      <div className="container">
+         <div className="n-container">
+          navbar
+         </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
